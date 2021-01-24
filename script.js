8cae0943099a5a38580ec11110629a312af18de9
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-24 15:32:07 +0530

    education and achievements partially ready

--- script.js
+++ script.js
@@ -72,6 +72,13 @@ $(document).ready(function(){
         loop: true
     });
 
+    var typed6 = new Typed(".typing-6", {
+        strings: ["Education", "Achivements", "Projects", "Hobbies"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
